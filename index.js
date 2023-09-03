5b6ed250fc21e7da67baf1d51473158b42401048
Andreas Tzionis <andy0n7@gmail.com>
2023-09-03 19:16:40 +0300

    cleanup

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
 const util = require('util')
+const crypto = require('crypto')
 
 const daLayer = []
-const executionLayer = {
-    rollups: {},
-    hub: { contracts: {}, sequencers: {} }
-}
+const executionLayer = { rollups: {}, hub: { contracts: {}, sequencers: {} } }
 
-const submitTransaction = (tx) => {
-    daLayer.push(tx)
-    processTransaction(tx)
-}
+const submitTransaction = (tx) => { daLayer.push(tx); processTransaction(tx) }
+const hash = (d) => crypto.createHash('md5').update(d).digest('hex')
 
 const processTransaction = (tx) => {
     if (tx.type === 'hub') {
         if (tx.action === 'create_contract') {
-            const contractId = 0 // TODO
             const rollupId = 0 // TODO
-            executionLayer['hub'].contracts[contractId] = { rollupId }
+            const contractId = hash(tx.data)
+            executionLayer['hub'].contracts[contractId] = { rollupId, code: tx.data }
         }
     } else if (tx.type === 'rollup') {
         if (tx.action === 'call_contract') {
-            
-            
-            const storage = { storage: { [tx.actionParams[0]]: 'updated_storage' }}
-
-            executionLayer.rollups[tx.typeParams[0]] = storage
+            const rollupId = tx.typeParams[0]
+            const updatedStorage = 'updated_storage' // TODO
+            const storage = { ...executionLayer.rollups[rollupId], storage: { [tx.actionParams[0]]: updatedStorage }}
+            executionLayer.rollups[rollupId] = storage
         }
     }
 }
 
 submitTransaction({type: 'hub', action: 'create_contract', data: '000402'})
-submitTransaction({type: 'rollup', typeParams: [0], action: 'call_contract', actionParams: [0], data: '01'})
+submitTransaction({type: 'rollup', typeParams: [0], action: 'call_contract', actionParams: [hash('000402')], data: '01'})
 
 console.log(util.inspect(executionLayer, {depth: null}))
