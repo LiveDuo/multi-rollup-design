84dd3c0187fe90d8c44ea9e3087f48be946963b9
Andreas Tzionis <andy0n7@gmail.com>
2023-09-03 21:18:04 +0300

    worked on evm

--- evm.js
+++ evm.js
@@ -1,11 +1,18 @@
-const { hexToBytes } = require('@ethereumjs/util')
 const { EVM } = require('@ethereumjs/evm')
+
+const OP_CODES = {STOP: '00', ADD: '01', PUSH1: '60'}
+const code = [OP_CODES.PUSH1, '03', OP_CODES.PUSH1, '05', OP_CODES.ADD, OP_CODES.STOP]
 
 ;(async () => {
 
     const evm = new EVM()
-    const result = await evm.runCode({ code: hexToBytes('0x01') })
-    console.log(result)
+    const result = await evm.runCode({ code: Buffer.from(code.join(''), 'hex') })
+    // console.log(Object.keys(result))
+    console.log(result.returnValue)
+    console.log(Buffer.from(result.runState.contract.storageRoot).toString('hex'))
+    console.log(Buffer.from(result.runState.contract.codeHash).toString('hex'))
+    console.log(result.gas)
 })()
 
 // https://www.npmjs.com/package/@ethereumjs/evm
+
