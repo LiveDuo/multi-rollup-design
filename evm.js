a165ef568809adb3e4ff0298f11a516d826e56d6
Andreas Tzionis <andy0n7@gmail.com>
2023-09-03 21:24:52 +0300

    worked on evm

--- evm.js
+++ evm.js
@@ -3,16 +3,19 @@ const { EVM } = require('@ethereumjs/evm')
 const OP_CODES = {STOP: '00', ADD: '01', PUSH1: '60'}
 const code = [OP_CODES.PUSH1, '03', OP_CODES.PUSH1, '05', OP_CODES.ADD, OP_CODES.STOP]
 
+// https://www.npmjs.com/package/@ethereumjs/evm
 ;(async () => {
 
     const evm = new EVM()
+    evm.events.on('step', (data) => console.log(`${data.opcode.name}\t-> ${data.stack}`))
+
     const result = await evm.runCode({ code: Buffer.from(code.join(''), 'hex') })
-    // console.log(Object.keys(result))
-    console.log(result.returnValue)
     console.log(Buffer.from(result.runState.contract.storageRoot).toString('hex'))
     console.log(Buffer.from(result.runState.contract.codeHash).toString('hex'))
-    console.log(result.gas)
+    console.log(result.executionGasUsed)
+    console.log(result.runState.stack)
+    console.log(result.returnValue)
+
+    // console.log(Object.keys(result))
+
 })()
-
-// https://www.npmjs.com/package/@ethereumjs/evm
-
