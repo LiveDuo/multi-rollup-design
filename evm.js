c53b8ddf9e1ca3d80d8f3145ce0f23ef53562ff1
Andreas Tzionis <andy0n7@gmail.com>
2023-09-03 21:28:10 +0300

    worked on evm

--- evm.js
+++ evm.js
@@ -3,18 +3,22 @@ const { EVM } = require('@ethereumjs/evm')
 const OP_CODES = {STOP: '00', ADD: '01', PUSH1: '60'}
 const code = [OP_CODES.PUSH1, '03', OP_CODES.PUSH1, '05', OP_CODES.ADD, OP_CODES.STOP]
 
+const evm = new EVM()
+
 // https://www.npmjs.com/package/@ethereumjs/evm
 ;(async () => {
 
-    const evm = new EVM()
+    console.log('---- Stack ----')
     evm.events.on('step', (data) => console.log(`${data.opcode.name}\t-> ${data.stack}`))
 
     const result = await evm.runCode({ code: Buffer.from(code.join(''), 'hex') })
-    console.log(Buffer.from(result.runState.contract.storageRoot).toString('hex'))
-    console.log(Buffer.from(result.runState.contract.codeHash).toString('hex'))
-    console.log(result.executionGasUsed)
-    console.log(result.runState.stack)
-    console.log(result.returnValue)
+    console.log()
+    console.log('---- Result ----')
+    console.log('storageRoot:', Buffer.from(result.runState.contract.storageRoot).toString('hex'))
+    console.log('codeHash', Buffer.from(result.runState.contract.codeHash).toString('hex'))
+    console.log('executionGasUsed', result.executionGasUsed)
+    console.log('stack', result.runState.stack._store[0])
+    console.log('returnValue', result.returnValue)
 
     // console.log(Object.keys(result))
 
