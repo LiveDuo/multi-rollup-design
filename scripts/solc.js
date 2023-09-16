6b5370ddf71ca61dbb3144fa7871507ae108bf47
Andreas Tzionis <andy0n7@gmail.com>
2023-09-16 13:01:35 +0300

    added calls to solc

--- scripts/solc.js
+++ scripts/solc.js
@@ -1,14 +1,57 @@
+const { TransactionFactory } = require('@ethereumjs/tx')
+const { Wallet } = require('@ethereumjs/wallet')
+const { VM } = require('@ethereumjs/vm')
+
+const keccak = require('ethereum-cryptography/keccak')
+
 const solc = require('solc')
 
 const filename = 'test.sol'
-const code = 'contract C { function f() public { } }'
+const code = 'contract C { uint public count = 1337; function get() public view returns (uint) { return count; } }'
+
+const senderWallet = Wallet.generate()
+
+const GAS_PRICE = '0x10', GAS_LIMIT = '0x20000'
+
+const vm = new VM()
 
 const input = {
   language: 'Solidity',
   sources: { [filename]: { content: code } },
   settings: { outputSelection: { '*': { '*': ['*'] } } }
 }
-const output = JSON.parse(solc.compile(JSON.stringify(input)))
-const compiled = output.contracts['test.sol']
-console.log(compiled['C'].evm.bytecode.object)
+
+; (async () => {
+  
+  // compile contract
+  const output = JSON.parse(solc.compile(JSON.stringify(input)))
+  const compiled = output.contracts[filename]
+  const contractData = compiled['C'].evm.bytecode.object
+
+  // deploy contract
+  const unsignedTx = TransactionFactory.fromTxData({ gasPrice: GAS_PRICE, gasLimit: GAS_LIMIT, data: '0x' + contractData, nonce: 0 })
+  const signedTx = unsignedTx.sign(senderWallet.getPrivateKey())
+  const result = await vm.runTx({ tx: signedTx, skipBalance: true })
+  const contractAddress = result.createdAddress
+  console.log('Contract Address: ', contractAddress.toString())
+
+  // read contract
+  const methodSignature = 'get()'
+  const methodIdBuffer = keccak.keccak256(Buffer.from(methodSignature)) //0xc7cee1b7
+  const methodId = '0x' + Buffer.from(methodIdBuffer).toString('hex').substring(0, 8)
+  const result2 = await vm.evm.runCall({ to: contractAddress, data: methodIdBuffer })
+  const returnValue = result2.execResult.returnValue
+  console.log('Read Return Value:', parseInt(Buffer.from(returnValue).toString('hex'), 16))
+
+  // call contract
+  const unsignedTx2 = TransactionFactory.fromTxData({ gasPrice: GAS_PRICE, gasLimit: GAS_LIMIT, data: methodId, nonce: 1 })
+  const signedTx2 = unsignedTx2.sign(senderWallet.getPrivateKey())
+  const result3 = await vm.runTx({ tx: signedTx2, skipBalance: true })
+  const returnValue2 = result3.execResult.runState.returnValue
+  console.log('Call Return Value:', returnValue2)
+
+  // debug
+  const contractStorage = await vm.stateManager.dumpStorage(contractAddress)
+  console.log(contractStorage)
+})()
 
