ca3465cad5889ba61b5c5ce918634e8581aba6d2
Andreas Tzionis <andy0n7@gmail.com>
2023-09-16 22:28:42 +0300

    worked on reassign

--- scripts/reassign.js
+++ scripts/reassign.js
@@ -21,18 +21,29 @@ const vm2 = new VM()
 	const signedTx = unsignedTx.sign(senderWallet.getPrivateKey())
 	const result = await vm1.runTx({ tx: signedTx, skipBalance: true })
 	
-	// debug contract 1
-	const storage = await vm1.stateManager.dumpStorage(result.createdAddress)
-	console.log('Address', result.createdAddress.toString(), '\n', storage)
-
 	// deploy contract 2
 	const code2 = [OP_CODES.PUSH1, '04', OP_CODES.PUSH1, '05', OP_CODES.SSTORE]
 	const unsignedTx2 = TransactionFactory.fromTxData({ gasPrice: GAS_PRICE, gasLimit: GAS_LIMIT, data: '0x' + code2.join(''), nonce: 0 })
 	const signedTx2 = unsignedTx2.sign(senderWallet2.getPrivateKey())
 	const result2 = await vm2.runTx({ tx: signedTx2, skipBalance: true })
 	
-	// debug contract 2
+	// reassign contract 1 to vm2
+	const account = await vm1.stateManager.getAccount(result.createdAddress)
+	await vm2.stateManager.putAccount(result.createdAddress, account)
+	const contractCode = await vm1.stateManager.getContractCode(result.createdAddress)
+	await vm2.stateManager.putContractCode(result.createdAddress, contractCode)
+	const contractStorage = await vm1.stateManager.dumpStorage(result.createdAddress)
+	for (const [k, v] of Object.entries(contractStorage)) {
+		const key = new Uint8Array(Buffer.from(k.substring(2), 'hex'))
+		const value = new Uint8Array(Buffer.from(v.substring(2), 'hex'))
+		// await vm2.stateManager.putContractStorage(result.createdAddress, key, value) // FIX
+	}
+	await vm1.stateManager.clearContractStorage(result.createdAddress)
+	
+	// debug contracts
+	const storage = await vm2.stateManager.dumpStorage(result.createdAddress)
 	const storage2 = await vm2.stateManager.dumpStorage(result2.createdAddress)
+	console.log('Address', result.createdAddress.toString(), '\n', storage)
 	console.log('Address', result2.createdAddress.toString(), '\n', storage2)
 
 })()
