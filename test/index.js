678c8812e24196f11ead619d87e2ee3d91382c78
Andreas Tzionis <andy0n7@gmail.com>
2023-10-08 12:18:42 +0300

    remove rollup id from queries

--- test/index.js
+++ test/index.js
@@ -15,25 +15,25 @@ test('create 2 contracts and reassign one of them', async () => {
 	const result2 = await submitTransaction({ type: 'hub', action: 'create_contract', data: '0x' + code2.join('') })
 
 	// call contract 1
-	await submitTransaction({ type: 'rollup', typeParams: [0], action: 'call_contract', actionParams: [result.createdAddress], data: '' })
-    const result3 = await queryState(0, result.createdAddress)
+	await submitTransaction({ type: 'rollup', action: 'call_contract', actionParams: [result.createdAddress], data: '' })
+    const result3 = await queryState(result.createdAddress)
     assert.strictEqual(Object.values(result3)[0], '0x02')
 
     // call contract 1
-	await submitTransaction({ type: 'rollup', typeParams: [0], action: 'call_contract', actionParams: [result.createdAddress], data: '' })
-    const result4 = await queryState(0, result.createdAddress)
+	await submitTransaction({ type: 'rollup', action: 'call_contract', actionParams: [result.createdAddress], data: '' })
+    const result4 = await queryState(result.createdAddress)
     assert.strictEqual(Object.values(result4)[0], '0x02')
 
     // call contract 2
-	await submitTransaction({ type: 'rollup', typeParams: [1], action: 'call_contract', actionParams: [result2.createdAddress], data: '' })
-    const result5 = await queryState(1, result2.createdAddress)
+	await submitTransaction({ type: 'rollup', action: 'call_contract', actionParams: [result2.createdAddress], data: '' })
+    const result5 = await queryState(result2.createdAddress)
     assert.strictEqual(Object.values(result5)[0], '0x04')
 
 	// reassign contract 2
 	await submitTransaction({ type: 'hub', action: 'reassign_contract', data: [result2.createdAddress, 0] })
 
     // call contract 2
-    const result6 = await queryState(0, result2.createdAddress)
+    const result6 = await queryState(result2.createdAddress)
     assert.strictEqual(Object.values(result6)[0], '0x04')
 
     // await debug()
