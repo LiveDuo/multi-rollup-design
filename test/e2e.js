860db87f4ac53fbf3178eab2217a724e68e42a03
Andreas Tzionis <andy0n7@gmail.com>
2023-10-10 13:55:14 +0300

    minor fix

--- test/e2e.js
+++ test/e2e.js
@@ -3,10 +3,10 @@ const assert = require('node:assert')
 const fetch = require('node-fetch')
 const { spawn } = require('node:child_process')
 
-const rpcRequest = async (method, params) => {
+const rpcRequest = async (url, method, params) => {
     const request = {jsonrpc: '2.0', method, id: 1, params}
     const requestParams = { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(request) }
-    const rawResponse = await fetch('http://127.0.0.1:8005', requestParams)
+    const rawResponse = await fetch(url, requestParams)
     const content = await rawResponse.json()
     return content.result
 }
@@ -18,7 +18,8 @@ test('e2e: create 2 contracts and reassign one of them', async () => {
     const node = spawn('node', ['index.js'])
     await new Promise(r => setTimeout(r, 2000)) // TODO wait ping
 
-    const res = await rpcRequest('echo', ['params'])
+    const url = `http://localhost:${8005}`
+    const res = await rpcRequest(url, 'echo', ['params'])
     assert.deepStrictEqual(res, ['params'])
 
     node.kill()
