57880dddb2a4e719d72d88e8f35818b80e3a1658
Andreas Tzionis <andy0n7@gmail.com>
2023-10-10 10:46:54 +0300

    cleanup

--- test/index.js
+++ test/index.js
@@ -26,25 +26,25 @@ test('create 2 contracts and reassign one of them', async () => {
 
 	// call contract 1
 	await submitTransaction({ type: 'rollup', action: 'call_contract', actionParams: [createResult.createdAddress], data: '' })
-    const callResult = await queryState(createResult.createdAddress)
-    assert.strictEqual(Object.values(callResult)[0], '0x02')
+    const stateData = await queryState(createResult.createdAddress)
+    assert.strictEqual(Object.values(stateData)[0], '0x02')
 
     // call contract 1
 	await submitTransaction({ type: 'rollup', action: 'call_contract', actionParams: [createResult.createdAddress], data: '' })
-    const callResult2 = await queryState(createResult.createdAddress)
-    assert.strictEqual(Object.values(callResult2)[0], '0x02')
+    const stateData2 = await queryState(createResult.createdAddress)
+    assert.strictEqual(Object.values(stateData2)[0], '0x02')
 
     // call contract 2
 	await submitTransaction({ type: 'rollup', action: 'call_contract', actionParams: [createResult2.createdAddress], data: '' })
-    const callResult3 = await queryState(createResult2.createdAddress)
-    assert.strictEqual(Object.values(callResult3)[0], '0x04')
+    const stateData3 = await queryState(createResult2.createdAddress)
+    assert.strictEqual(Object.values(stateData3)[0], '0x04')
 
 	// reassign contract 2
 	await submitTransaction({ type: 'hub', action: 'reassign_contract', data: [createResult2.createdAddress, 0] })
 
     // call contract 2
-    const result6 = await queryState(createResult2.createdAddress)
-    assert.strictEqual(Object.values(result6)[0], '0x04')
+    const stateData4 = await queryState(createResult2.createdAddress)
+    assert.strictEqual(Object.values(stateData4)[0], '0x04')
 
     // await debug()
 
