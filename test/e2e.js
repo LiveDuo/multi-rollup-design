b158bf9c1c9ef0bfba9406a788a46c523c082a7a
Andreas Tzionis <andy0n7@gmail.com>
2023-10-10 12:32:25 +0300

    added e2e test

---
+++ test/e2e.js
@@ -0,0 +1,26 @@
+const test = require('node:test')
+const assert = require('node:assert')
+const fetch = require('node-fetch')
+const { spawn } = require('node:child_process')
+
+const rpcRequest = async (method, params) => {
+    const request = {jsonrpc: '2.0', method, id: 1, params}
+    const requestParams = { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(request) }
+    const rawResponse = await fetch('http://127.0.0.1:8005', requestParams)
+    const content = await rawResponse.json()
+    return content.result
+}
+
+// node --test test/e2e.js
+test('e2e: create 2 contracts and reassign one of them', async () => {
+    
+    // start node
+    const node = spawn('node', ['index.js'])
+    await new Promise(r => setTimeout(r, 2000)) // TODO wait ping
+
+    const res = await rpcRequest('echo', ['params'])
+    assert.deepStrictEqual(res, ['params'])
+
+    node.kill()
+
+})
