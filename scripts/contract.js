11dd6724d01379eb5dda5f3599abf8ffafa99391
Andreas Tzionis <andy0n7@gmail.com>
2023-10-14 17:10:23 +0300

    added contract script

---
+++ scripts/contract.js
@@ -0,0 +1,9 @@
+
+const { Address } = require('@ethereumjs/util')
+
+const senderAddress = Address.fromString(process.argv[2])
+const nonce = BigInt(process.argv[3])
+
+// node scripts/contract.js 0xb363821c8ebd9b327f03965bc91555f7ea42157a 0
+const address = Address.generate(senderAddress, nonce)
+console.log(address.toString())
